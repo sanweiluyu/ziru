804877aa577b6707fe3b9e0fc89176c9e33f525e
爹 <704841162@qq.com>
2018-12-26 21:31:43 +0800

    提交登录注册页面

---
+++ application/index/controller/Sign.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 2018/12/26
+ * Time: 下午7:48
+ */
+
+namespace app\index\controller;
+
+use think\Controller;
+
+class Sign extends Controller
+{
+    public function in()
+    {
+        return view();
+    }
+
+    public function up()
+    {
+        return view();
+    }
+}
