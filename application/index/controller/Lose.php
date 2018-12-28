182543eee44d85ce7f7ee662ca5fcce566b11d82
爹 <704841162@qq.com>
2018-12-28 21:05:31 +0800

    提交登录，注册，丢失密码页面

---
+++ application/index/controller/Lose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qingyun
+ * Date: 2018/12/28
+ * Time: 下午8:37
+ */
+namespace app\index\controller;
+
+use think\Controller;
+
+class Lose extends Controller
+{
+    public function password()
+    {
+        return view();
+    }
+}
