1c7b9a9ea7d1fb08f239b9c2ede58d05bd59d3bc
山大王 <921703305@qq.com>
2018-12-29 09:37:14 +0800

    wu2

--- config/database.php
+++ config/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '192.168.12.156',
     // 数据库名
-    'database'        => '',
+    'database'        => 'ziru',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'wu',
     // 密码
-    'password'        => '',
+    'password'        => '123456',
     // 端口
     'hostport'        => '',
     // 连接dsn
